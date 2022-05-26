791d36c1e07916d4e7b8ea93faca00e4b27475e9
Kane191 <sarakane17@gmail.com>
2022-05-26 15:15:35 +0300

    create posts css

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import Axios from 'axios';
+import './CreatePost.css';
+import carrot from '../images/carrot.png';
+
 
 function CreatePost() {
     const [userId, setUserId] = useState("");
@@ -7,23 +10,37 @@ function CreatePost() {
     const [post, setPost] = useState("");
 
     const submitPost = () => {
-        Axios.post('http://localhost:3002/api/create', {userId : userId, title : title, post : post})
+        Axios.post('http://localhost:3002/api/create', {userId : userId, title : title, post : post});
     }
 
     return (
-        <div className="CreatePost">
-            <div>
-                <label>Userid: </label>
-                <input type="text" onChange={(e)=>{  setUserId(e.target.value)  }}/><br/>
-
-                <label>Title: </label>
-                <input type="text" onChange={(e)=>{  setTitle(e.target.value)  }}/><br/>
-
-                <label>Post: </label>
-                <textarea onChange={(e)=>{  setPost(e.target.value)  }}></textarea><br/>
-
-                <button onClick={submitPost}>Submit Post</button>
+        <div className="CreatePost row">
+            <div className="col form_bg">
+                <h1>TOKKI BLOG</h1>
+                <div className="d-flex">
+                    <p>Create a <span>POST</span></p>
+                    <img alt="" src={carrot} height="60" className="logo_image"/>
+                </div>
             </div>
+
+            <div className="col form_r">
+                {/* <label>Title: </label> */}
+                <input className="form-control" placeholder="Title" type="text" onChange={(e)=>{  setTitle(e.target.value)  }}/><br/>
+
+                {/* <label>Post: </label> */}
+                <textarea className="form-control" placeholder="Body" onChange={(e)=>{  setPost(e.target.value)  }}></textarea><br/>
+
+                <input placeholder="Select an image" className="form-control" type="file" id="formFile"></input>
+
+                <button className="btn btn-primary " onClick={submitPost}>Submit Post</button>
+            </div>
+
+            {
+                /* 
+                <label>Userid: </label>
+                <input type="text" onChange={(e)=>{  setUserId(e.target.value)  }}/><br/> 
+                */
+            }                
         </div>
     )
 }
